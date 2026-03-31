f995945da1cf50a92f04c6d49e10327f58063f9a
SUDEESH JOHN <sudeeshjohn@in.ibm.com>
2026-03-31 07:42:20 +0530

    Improve boolean display formatting for FC and iSCSI backed fields
    - Changed from %v format specifier to user-friendly Yes/No format
    - Added map-based boolean to string conversion for better readability
    - Output now shows 'Yes' or 'No' instead of 'true' or 'false'
    - Improves user experience when viewing physical volume backing information

--- examples/getviosscsimappings/main.go
+++ examples/getviosscsimappings/main.go
@@ -207,13 +207,15 @@ func displayAllResults(results []viosMappingResult) {
 				physicalCount++
 				pv := mapping.Storage.PhysicalVolume
 				fmt.Printf("   Volume Name:    %s\n", pv.VolumeName)
-				fmt.Printf("   Capacity:       %d\n", pv.VolumeCapacity)
+				fmt.Printf("   Capacity:       %v\n", pv.VolumeCapacity)
 				fmt.Printf("   Volume State:   %s\n", pv.VolumeState)
 				fmt.Printf("   Volume UDID:    %s\n", pv.VolumeUniqueID)
 				fmt.Printf("   Description:    %s\n", pv.Description)
 				fmt.Printf("   Location Code:  %s\n", pv.LocationCode)
-				fmt.Printf("   FC Backed:      %t\n", pv.IsFibreChannelBacked)
-				fmt.Printf("   iSCSI Backed:   %t\n", pv.IsISCSIBacked)
+				// Convert boolean to user-friendly Yes/No format
+				boolStr := map[bool]string{true: "Yes", false: "No"}
+				fmt.Printf("   FC Backed:      %s\n", boolStr[pv.IsFibreChannelBacked])
+				fmt.Printf("   iSCSI Backed:   %s\n", boolStr[pv.IsISCSIBacked])
 				fmt.Printf("   Reserve Policy: %s\n", pv.ReservePolicy)
 				if pv.StorageLabel != "" {
 					fmt.Printf("   Storage Label:  %s\n", pv.StorageLabel)
