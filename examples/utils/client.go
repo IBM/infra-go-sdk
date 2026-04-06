84807500eb1f34330e8d7c094142a29beadd054c
SUDEESH JOHN <sudeeshjohn@in.ibm.com>
2026-04-06 21:20:43 +0530

    fix: remove godotenv dependency from example client

--- examples/utils/client.go
+++ examples/utils/client.go
@@ -4,18 +4,17 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/sudeeshjohn/svc-go-sdk"
 )
 
 func GetSVCClient() *svc.Client {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	ip := os.Getenv("SVC_IP")
 	username := os.Getenv("SVC_USERNAME")
 	password := os.Getenv("SVC_PASSWORD")
+
+	if ip == "" || username == "" || password == "" {
+		log.Fatal("missing required environment variables: SVC_IP, SVC_USERNAME, SVC_PASSWORD")
+	}
+
 	return svc.NewClient(ip, username, password).WithTLSInsecure()
 }
